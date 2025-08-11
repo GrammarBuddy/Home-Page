295e8dc00a45b6a9d8409241ba768e96dc617176
GrammarBuddy <english3sixty.in@gmail.com>
2025-08-11 18:08:53 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,6 +5,7 @@ const urlsToCache = [
   '/styles.css',
   '/script.js'
 ];
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyx916H_72LsiU9IV7LiFnZJ_wYNyWKbLMTBgvWvN6ZKGy51rc3-ei7nE6mDSLJ3H2f/exec';
 
 self.addEventListener('install', event => {
   event.waitUntil(
@@ -17,6 +18,12 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
+  if (event.request.url.startsWith(SCRIPT_URL)) {
+    // This is the POST request to the Google Script.
+    // Do not cache it, and allow it to go directly to the network.
+    return;
+  }
+  
   event.respondWith(
     caches.match(event.request)
       .then(response => {

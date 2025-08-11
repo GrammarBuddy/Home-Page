8c8f0b62974eaf6a5cf776fa0f9555802ccc5e7a
GrammarBuddy <english3sixty.in@gmail.com>
2025-08-11 17:38:30 +0530

    Update and rename service-worker.js to sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "grammarbuddy-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+  // Add other important files like CSS, JS, images if needed
+];
+
+// Install Service Worker & Cache Files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate Service Worker & Remove Old Caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch From Cache, Fallback to Network
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});

79e1bef8158d212cedef372c6cebf1592eef6674
GrammarBuddy <english3sixty.in@gmail.com>
2025-08-16 19:16:37 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "grammarbuddy-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/style.css",   // your only CSS file
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+// Install service worker & cache files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update cache when new version is deployed
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
